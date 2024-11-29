0f7f05e123127b3c8148e00e1be9a6c9c520d0f9
peplxx <seregaopkymelnikov@yandex.ru>
2024-11-29 22:51:40 +0300

    feat: multilayered grid system

--- mahjong-game/src/components/grid.tsx
+++ mahjong-game/src/components/grid.tsx
@@ -1,36 +1,79 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 type GridProps = {
-  columns: number
-  rows: number
-  gap: number
-  height: number
-  width: number
+  columns: number;
+  rows: number;
+  gap: number;
+  height: number;
+  width: number;
+  style?: React.CSSProperties;
+};
+
+type GridCell = {
+    isSelected: boolean;
 }
 
-const StyledGrid = styled.div<GridProps>`
-  display: grid;
-  ${({ columns, rows, gap, height, width }) => css`
-    grid-template-columns: repeat(${columns}, ${width}px);
-    grid-template-rows: repeat(${rows}, ${height}px);
-    grid-gap: ${gap}px;
-    justify-content: center;
-    align-content: center;
-    background-color: #282c34;
-    height: 100vh;
-  `}
-`
+const LayerContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
 
-const Grid = ({ columns, rows, gap}: GridProps) => {
+const StyledGrid = styled(motion.div)<GridProps>`
+  display: grid;
+  grid-template-columns: repeat(${({ columns }) => columns}, ${({ width }) => width}px);
+  grid-template-rows: repeat(${({ rows }) => rows}, ${({ height }) => height}px);
+  grid-gap: ${({ gap }) => gap}px;
+  justify-content: center;
+  align-content: center;
+  background-color: rgba(40, 44, 52, 0.5);
+  position: absolute; 
+  top: 0;
+  height: 100%;
+  width: 100%;
+  left: 0;
+`;
+
+const StyleGridCell = styled(motion.div)<GridCell>`
+  background-color: ${({ isSelected }) => isSelected ? 'rgba(40, 44, 52, 0.5)' : 'red'};
+`;
+
+const Grid = ({ columns, rows, gap, height, width, style }: GridProps) => {
+  const [selectedCells, setSelectedCells] = useState<{[key: number]: boolean}>({});
+
+  const onCellClick = (index: number) => {
+    setSelectedCells((selectedCells) => {
+      return {
+        ...selectedCells,
+        [index]: !selectedCells[index],
+      };
+    });
+  };
+
   return (
-    <StyledGrid columns={columns} rows={rows} gap={gap} height={150} width={100}>
+    <StyledGrid columns={columns} rows={rows} gap={gap} height={height} width={width} style={style}>
       {[...Array(columns * rows)].map((_, index) => (
-        <div key={index} className="grid-cell" style={{ backgroundColor: 'white' }}></div>
+        <StyleGridCell onClick={() => onCellClick(index)} 
+        isSelected={selectedCells[index]} key={index} 
+        whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} whileDrag={{ scale: 0.95 }}
+         drag dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }} />
       ))}
     </StyledGrid>
-  )
-}
+  );
+};
 
-export default Grid
+const MultiLayerGrid = () => {
+  return (
+    <LayerContainer>
+      <Grid columns={10} rows={6} gap={5} height={150} width={100} style={{ zIndex: 1, backgroundColor: '#41454C' }} />
+      <Grid columns={9} rows={5} gap={5} height={150} width={100} style={{ zIndex: 2}} />
+      <Grid columns={8} rows={4} gap={5} height={150} width={100} style={{ zIndex: 3}} />
+
+    </LayerContainer>
+  );
+};
+
+export default MultiLayerGrid;
 
--- mahjong-game/src/index.tsx
+++ mahjong-game/src/index.tsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Grid from './components/grid';
+import MultiLayerGrid from './components/grid';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Grid columns={6} rows={6} gap={5} height={50} width={50} />
+    <div style={{position: 'relative'}}>
+      <MultiLayerGrid />
+    </div>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
