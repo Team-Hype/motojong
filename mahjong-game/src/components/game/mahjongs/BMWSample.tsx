6e391ae75f923d56ac149af9288df6e2958fc38e
peplxx <seregaopkymelnikov@yandex.ru>
2024-11-30 00:50:00 +0300

    feat: BMW Mahjongs sample

--- mahjong-game/src/components/game/mahjongs/BMWSample.tsx
+++ mahjong-game/src/components/game/mahjongs/BMWSample.tsx
@@ -6,8 +6,34 @@ export class BMWHexagon implements Mahjong {
     image = "bmw/BMW-hexagon.png";
 }
 
-
 export class BMWLogo implements Mahjong{
     state = "avaliable";
     image = "bmw/BMW-logo.png";
 }
+export class BMWM3 implements Mahjong{
+    state = "avaliable";
+    image = "bmw/BMW-m3_car.png";
+}
+
+export class BMWM4 implements Mahjong{
+    state = "avaliable";
+    image = "bmw/BMW-m4-car.png";
+}
+export class BMWX implements Mahjong{
+    state = "avaliable";
+    image = "bmw/BMW-x-car.png";
+}
+export class BMWBlackCar implements Mahjong{
+    state = "avaliable";
+    image = "bmw/BMW-black-car.png";
+}
+
+
+export const BMWSample : Mahjong[] = [
+    new BMWHexagon(),
+    new BMWLogo(),
+    new BMWM3(),
+    new BMWM4(),
+    new BMWX(),
+    new BMWBlackCar()
+]
